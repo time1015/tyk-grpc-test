cd02a247a66c49bf3ba60063fea88b3044f7bdab
John Daniel Regino <time1015@users.noreply.github.com>
2019-12-17 00:47:20 +0800

    Set up server in Main

--- src/main/java/io/se7en/grpctest/Main.java
+++ src/main/java/io/se7en/grpctest/Main.java
@@ -1,7 +1,31 @@
 package io.se7en.grpctest;
 
-public class Main {
-  public static void main(String[] args) {
+import java.io.IOException;
+import java.util.Properties;
 
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+public class Main {
+  private static final Properties PROPERTIES = loadProperties();
+
+  private static Properties loadProperties() {
+    Properties props = new Properties();
+    props.putAll(System.getProperties());
+    return props;
+  }
+
+  public static void main(String[] args) throws InterruptedException, IOException {
+    int port = port();
+
+    System.out.println("Starting server at port " + port + "...");
+    Server server = ServerBuilder.forPort(port).build().start();
+    System.out.println("Server started!");
+
+    server.awaitTermination();
+  }
+
+  private static int port() {
+    return Integer.parseInt(PROPERTIES.getProperty("port", "8090"));
   }
 }
