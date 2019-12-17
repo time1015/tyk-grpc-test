fed088b4110899ca73846bf8e9078713eb7ff414
John Daniel Regino <time1015@users.noreply.github.com>
2019-12-17 10:10:54 +0800

    Fetch hooks from registry

--- src/main/java/io/se7en/grpctest/Dispatcher.java
+++ src/main/java/io/se7en/grpctest/Dispatcher.java
@@ -1,12 +1,31 @@
 package io.se7en.grpctest;
 
+import java.util.Optional;
+import java.util.function.Function;
+
 import coprocess.CoprocessObject.Object;
 import coprocess.DispatcherGrpc.DispatcherImplBase;
 import io.grpc.stub.StreamObserver;
 
 public final class Dispatcher extends DispatcherImplBase {
+  private static final Function<Object, Optional<Object>> TRANSFORMER = HookRegistry.getTransformer();
+
   @Override
   public void dispatch(Object request, StreamObserver<Object> responseObserver) {
+    printRequest("Request: ", request);
 
+    Optional<Object> newRequest = TRANSFORMER.apply(request);
+
+    responseObserver.onNext(newRequest.orElse(request));
+    newRequest.ifPresent(r -> printRequest("Final Request to Upstream: ", r));
+
+    responseObserver.onCompleted();
+  }
+
+  private void printRequest(String header, Object request) {
+    System.out.println(header);
+    System.out.println("===REQUEST START===");
+    System.out.println(request);
+    System.out.println("====REQUEST END====");
   }
 }
